69c4cb7f728895e15a6d2761ea148a67ff29ef99
zhou2004jj <12854881+zhou2004jj@user.noreply.gitee.com>
2025-10-02 17:49:25 +0800

    db2.0 add gene_express

--- backend/db/repositories/GeneExpressRepository.py
+++ backend/db/repositories/GeneExpressRepository.py
@@ -2,7 +2,7 @@ from backend.db.models.entity.GeneExpress import GeneExpress
 from .base import BaseRepository
 from abc import ABC,abstractmethod
 
-class GeneExpressCountsRepository(ABC, BaseRepository[GeneExpress]):
+class GeneExpressRepository(ABC, BaseRepository[GeneExpress]):
     def __init__(self):
         super().__init__(GeneExpress)
 
